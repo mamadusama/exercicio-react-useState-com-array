31b3a40b757131fb397209ae9ed8537fd4944801
mamadusama <seu_email@example.commamadusamadev@gmail.com>
2024-07-31 21:44:25 +0100

    Add useState with array, building simple task list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("")
+  const [tarefas, setTarefas] = useState([]);
+
+
+  function handelTarefas(e){
+    e.preventDefault();
+    alert(`${input}  adicionado com sucesso`);
+    setTarefas([...tarefas, input]);
+
+    setInput("");
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Lista tarefa</h1>
+
+      <div>
+        <form onSubmit={handelTarefas}>
+          <label>Adicionar tarefa</label><br></br>
+          <input
+            placeholder="adiciiionar tarefa"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          /><br></br>
+
+          <button type="submit">adicionar</button>
+        </form>
+
+        <ul>
+      {tarefas.map((tarefa) => (
+            <li>{tarefa}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
